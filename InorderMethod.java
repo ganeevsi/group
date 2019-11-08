dbe0bef7c5db7e37a5bb70f61a0d92a8aa13f292
gargdivyam327 <gargdivyam327@gmail.com>
2019-11-08 10:25:01 +0530

    Inorder method updated

---
+++ InorderMethod.java
@@ -0,0 +1,60 @@
+class Node 
+{ 
+    int key; 
+    Node left, right; 
+  
+    public Node(int item) 
+    { 
+        key = item; 
+        left = right = null; 
+    } 
+} 
+  
+public class InorderMethod
+{ 
+    // Root of Binary Tree 
+    Node root; 
+  
+    InorderMethod() 
+    { 
+        root = null; 
+    } 
+    void printInorder(Node node) 
+    { 
+        if (node == null) 
+            return; 
+  
+        /* first recur on left child */
+        printInorder(node.left); 
+  
+        /* then print the data of node */
+        System.out.print(node.key + " "); 
+  
+        /* now recur on right child */
+        printInorder(node.right); 
+    } 
+
+    void printInorder()   
+     {     
+         printInorder(root);  
+     }
+
+     public static void main(String[] args) 
+    { 
+      InorderMethod tree = new InorderMethod(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+  
+        System.out.println("Preorder traversal of binary tree is "); 
+        tree.printPreorder(); 
+  
+        System.out.println("\nInorder traversal of binary tree is "); 
+        tree.printInorder(); 
+  
+        System.out.println("\nPostorder traversal of binary tree is "); 
+        tree.printPostorder(); 
+    } 
+} 
